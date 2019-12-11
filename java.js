654f0263a5e30444fc52989349b7350c7612dd45
bmoreno304 <45017629+bmoreno304@users.noreply.github.com>
2019-12-11 12:44:13 -0800

    Add files via upload

---
+++ java.js
@@ -0,0 +1,16 @@
+var a = document.getElementById("ps4");
+var b = document.getElementById("gon");
+var c = document.getElementById("turkey");
+
+function changeImg () {
+a.classList.add("gone");
+}
+
+function changeImg1 () {
+b.classList.add("gone");
+}
+
+function changeImg2 () {
+c.classList.add("gone");
+}
+
